48055ada666b51c260711d53baf96b7ebfd42086
Basant Chaudhary <basant.codewing@gmail.com>
2023-01-06 14:12:04 +0545

    init: app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import "./index.css";
+
+const domContainer = document.getElementById('root');
+const root = createRoot(domContainer)
+
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
