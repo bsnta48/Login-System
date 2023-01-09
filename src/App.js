7d385d6c74807f200b5376c52490c6e0a82e88d4
Basant Chaudhary <basant.codewing@gmail.com>
2023-01-09 09:23:40 +0545

    feat: login form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='flex'>Hello World!</div>
+    <div className='max-w-[1170px] px-4 mx-auto w-full'>
+      <div className='h-screen flex items-center'>
+        <form className='w-full max-w-[500px] mx-auto bg-gray-100 p-8 rounded-md space-y-8 drop-shadow-md'>
+          <input type="email" className='w-full p-4 rounded-md' placeholder='Enter your email address' />
+          <input type="password" className='w-full p-4 rounded-md' placeholder='Enter your password' />
+          <input type="submit" className='p-4 bg-slate-500 text-white cursor-pointer rounded-md hover:bg-slate-600 transition' value="Login" />
+        </form>
+      </div>
+    </div>
   )
 }
 
