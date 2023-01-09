2547fd257e2b848ed86fd182b22bd25a4bc04c37
Basant Chaudhary <basant.codewing@gmail.com>
2023-01-09 15:55:19 +0545

    style: login ui done

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ const App = () => {
     <div className='max-w-[1170px] px-4 mx-auto w-full'>
       <div className='h-screen flex items-center'>
         <form className='w-full max-w-[500px] mx-auto bg-gray-100 p-8 rounded-md space-y-8 drop-shadow-md'>
-          <input type="email" className='w-full p-4 rounded-md' placeholder='Enter your email address' />
-          <input type="password" className='w-full p-4 rounded-md' placeholder='Enter your password' />
-          <input type="submit" className='p-4 bg-slate-500 text-white cursor-pointer rounded-md hover:bg-slate-600 transition' value="Login" />
+          <label htmlFor='username' className='block'>
+            Username:
+            <input id="username" type="email" className='w-full p-4 rounded-md' placeholder='Enter your email address' autoComplete='off' />
+          </label>
+          <label htmlFor='password' className='block'>
+            Password:
+            <input id="password" type="password" className='w-full p-4 rounded-md' placeholder='Enter your password' autoComplete='off' />
+          </label>
+          <input type="submit" className='p-4 bg-sky-500 text-white cursor-pointer rounded-md hover:bg-sky-600 transition' value="Login to your account" />
         </form>
       </div>
     </div>
